66e8e4dbe4fe07ead18ff6cb5b3865643885f9c7
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-08 10:41:23 +0300

    add a database, add and initialize the Migrate class

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     MOVIE_API_BASE_URL = "https://api.themoviedb.org/3/movie/{}?api_key={}"
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLACHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Elm1n10@locathost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Elm1n10@localhost/watchlist'
 
 class prodConfig(Config):
     """
